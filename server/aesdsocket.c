fc8ddf17e656f65cc19537e28245becc31e424c8
Joshua Roberson <joshua.roberson@colorado.edu>
2026-07-04 16:10:31 -0600

    Implemented changes for assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,6 +11,8 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/queue.h>
+#include <pthread.h>
 
 #define AESD_SOCK_PORT "9000"
 #define LISTEN_BACKLOG 10
@@ -19,17 +21,149 @@
 #define INITIAL_BUFFER_SIZE 512
 
 int running = 1; //global flag to shutdown when SIGINT or SIGTERM is received 
+pthread_mutex_t list_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t file_write_mutex = PTHREAD_MUTEX_INITIALIZER;
+SLIST_HEAD(connection_list, connection_node) head = SLIST_HEAD_INITIALIZER(head);
+
+//connection node structure
+typedef struct connection_node {
+    int client_fd;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    int is_complete;
+    pthread_t thread_id;
+    SLIST_ENTRY(connection_node) entries;
+} connection_node_t;
+
 
 void signal_handler(int signal){
     if(signal == SIGINT || signal == SIGTERM){
-        syslog(LOG_INFO, "Caught signal, exiting");
         running = 0;
     }
 }
 
+void *timer_handler(void *args){
+    while (running){
+        for (int i = 0; i < 10 && running; i++){
+            sleep(1);
+        }
+        if (!running) break;
+
+        time_t now = time(NULL); //gets raw linux timestamp
+        struct tm *timestamp_struct = localtime(&now);
+
+        char timestamp[100];
+        strftime(timestamp, sizeof(timestamp), "timestamp:%a, %d %b %Y %H:%M:%S %z\n", timestamp_struct);
+
+        pthread_mutex_lock(&file_write_mutex);
+        int fd = open(WRITE_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd != -1){
+            write(fd, timestamp, strlen(timestamp));
+            close(fd);
+        }
+        pthread_mutex_unlock(&file_write_mutex);
+    }
+    return NULL;
+}
+
+void *connection_handler(void *args){
+    ssize_t received, read_count;
+    size_t buf_size = INITIAL_BUFFER_SIZE; //initial buffer size
+    size_t total_received = 0; //number of bytes received into buffer
+    int writefile_fd, readfile_fd;
+    int newline = 0;
+    char ip_string[INET6_ADDRSTRLEN];
+
+    connection_node_t *connection = (connection_node_t *)args;
+    
+    char *buffer=malloc(INITIAL_BUFFER_SIZE);
+    if (buffer == NULL){
+        syslog(LOG_ERR, "Unable to allocate memory for buffer.");
+        connection->is_complete = 1;
+        return NULL;
+    }
+
+    //log successful connection
+        getnameinfo((struct sockaddr *)&connection->their_addr, connection->addr_size, ip_string, sizeof(ip_string), NULL, 0, NI_NUMERICHOST);
+        syslog(LOG_INFO, "Accepted connection from %s", ip_string);
+
+        total_received = 0; //number of bytes received into buffer
+        newline = 0;
+        while(!newline){
+            received = recv(connection->client_fd, buffer + total_received, buf_size - total_received, 0);
+            total_received += received;
+            
+            syslog(LOG_DEBUG, "total received: %zu", total_received);
+
+            if (received == -1) {
+                syslog(LOG_ERR, "receive error:%s\n", strerror(errno));
+                free(buffer);
+                close(connection->client_fd);
+                connection->is_complete = 1;
+                return NULL;
+            }
+
+            //check for newline
+            for (int i = total_received - received; i<total_received; i++){
+                if(buffer[i] == '\n'){
+                    newline = 1;
+                    break;
+                }
+            }
+
+            //no newline yet, grow the buffer
+            if (!newline && total_received == buf_size){
+                buf_size *= 2;
+                char *new_buf = realloc(buffer, buf_size);
+                if (new_buf == NULL){ //makes sure realloc succeeds, and frees buffer if it doesn't to prevent memory leak
+                    syslog(LOG_ERR, "Error growing buffer");
+                    free(buffer);
+                    close(connection->client_fd);
+                    connection->is_complete = 1;
+                    return NULL;
+                }
+                buffer = new_buf;
+            }
+            
+            //syslog(LOG_DEBUG, "buffer %s", buffer);
+
+            if(received == 0) //if the client closed the connection, stop receiving 
+                break;
+
+        }
+
+        pthread_mutex_lock(&file_write_mutex);
+        writefile_fd = open(WRITE_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        write(writefile_fd, buffer, total_received);
+        close(writefile_fd);
+        readfile_fd = open(READ_FILE, O_RDONLY);
+
+        do{
+            read_count = read(readfile_fd, buffer, buf_size);
+            
+            if (read_count == -1) {
+                syslog(LOG_ERR, "read file error:%s\n", strerror(errno));
+                break;
+            }
+
+            if(read_count)
+                send(connection->client_fd, buffer, read_count, 0);
+
+        } while (read_count);
+
+        close(readfile_fd);
+        pthread_mutex_unlock(&file_write_mutex);
+        free(buffer);
+        close(connection->client_fd);
+        syslog(LOG_INFO, "Closed connection from %s", ip_string);
+        connection->is_complete = 1;
+
+        return NULL;
+}
+
 int main(int argc, char *argv[]){
     //Start a logger
-	openlog("Assignment5", 0, LOG_USER);
+	openlog("Assignment6", 0, LOG_USER);
 
     //setup signal handler
     struct sigaction signal_action;
@@ -38,28 +172,17 @@ int main(int argc, char *argv[]){
     sigaction(SIGINT, &signal_action, NULL);
     sigaction(SIGTERM, &signal_action, NULL);
     
-    int status, sock_fd, their_fd, writefile_fd, readfile_fd;
-    int newline = 0;
+    int status, sock_fd, their_fd;
     struct addrinfo hints;
     struct addrinfo *servinfo;
     struct sockaddr_storage their_addr;
-    char ip_string[INET6_ADDRSTRLEN];
     socklen_t addr_size;
-    char *buffer=malloc(INITIAL_BUFFER_SIZE);
-    if (buffer == NULL){
-        syslog(LOG_ERR, "Unable to allocate memory for buffer.");
-        exit(1);
-    }
+    connection_node_t *node, *next_node;
     
-    ssize_t received, read_count;
-    size_t buf_size = INITIAL_BUFFER_SIZE; //initial buffer size
-    size_t total_received = 0; //number of bytes received into buffer
-
     memset(&hints, 0, sizeof(hints));   //make sure the hints struct is empty
     hints.ai_family = AF_INET;          //IPv4
     hints.ai_socktype = SOCK_STREAM;    //TCP Stream Socket
     hints.ai_flags = AI_PASSIVE;        //fill in the IP for me
-
 
     status = getaddrinfo(NULL, AESD_SOCK_PORT, &hints, &servinfo);
 
@@ -84,7 +207,6 @@ int main(int argc, char *argv[]){
         syslog(LOG_ERR, "bind error:%s\n", strerror(errno));
         exit(4);
     }
-
 
     //After binding, but before moving on, check if argument specified for daemon and start daemon if so
     if( (argc == 2) && (strcmp(argv[1], "-d") == 0) ){
@@ -115,6 +237,9 @@ int main(int argc, char *argv[]){
 
     addr_size = sizeof(their_addr);
     
+    pthread_t timer_thread;
+    pthread_create(&timer_thread, NULL, timer_handler, NULL);
+
     while(running){
         their_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &addr_size);
         //addr_size is a pointer because it takes size of sockaddr_storage as an input and outputs actual size of the addr filled in
@@ -122,83 +247,62 @@ int main(int argc, char *argv[]){
             //guaranteed to be castable to sockaddr
 
         if (their_fd == -1) { //if there is an error in accept other that signal interrupt, exit program with error
-            if(errno == EINTR) break;
+            if(errno == EINTR) break; //if errno is EINTR, then a signal was received, exit the loop
             else{
                 syslog(LOG_ERR, "accept error:%s\n", strerror(errno));
                 exit(6);
             }
         }
 
-        //log successful connection
-        getnameinfo((struct sockaddr *)&their_addr, addr_size, ip_string, sizeof(ip_string), NULL, 0, NI_NUMERICHOST);
-        syslog(LOG_INFO, "Accepted connection from %s", ip_string);
-
-        total_received = 0; //number of bytes received into buffer
-        newline = 0;
-        while(!newline){
-            received = recv(their_fd, buffer + total_received, buf_size - total_received, 0);
-            total_received += received;
-            
-            syslog(LOG_DEBUG, "total received: %zu", total_received);
-
-            if (received == -1) {
-                syslog(LOG_ERR, "receive error:%s\n", strerror(errno));
-                exit(7);
-            }
-
-            //check for newline
-            for (int i = total_received - received; i<total_received; i++){
-                if(buffer[i] == '\n'){
-                    newline = 1;
-                    break;
-                }
-            }
-
-            //no newline yet, grow the buffer
-            if (!newline){
-                buf_size *= 2;
-                char *new_buf = realloc(buffer, buf_size);
-                if (new_buf == NULL){ //makes sure realloc succeeds, and frees buffer if it doesn't to prevent memory leak
-                    free(buffer); 
-                    syslog(LOG_ERR, "Error growing buffer");
-                    exit(8);
-                }
-                buffer = new_buf;
-            }
-            
-            //syslog(LOG_DEBUG, "buffer %s", buffer);
-
-            if(received == 0) //if the client closed the connection, stop receiving 
-                break;
-
+        connection_node_t *new_conn = malloc(sizeof(connection_node_t));
+        if (new_conn == NULL){
+            syslog(LOG_ERR, "Unable to allocate memory for new connection node.");
+            exit(11);
         }
 
-        writefile_fd = open(WRITE_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        write(writefile_fd, buffer, total_received);
-        close(writefile_fd);
-        readfile_fd = open(READ_FILE, O_RDONLY);
+        new_conn->client_fd = their_fd;
+        new_conn->their_addr = their_addr;
+        new_conn->addr_size = addr_size;
+        new_conn->is_complete = 0;
 
-        do{
-            read_count = read(readfile_fd, buffer, buf_size);
-            
-            if (read_count == -1) {
-                syslog(LOG_ERR, "read file error:%s\n", strerror(errno));
-                exit(9);
+        pthread_create(&new_conn->thread_id, NULL, connection_handler, new_conn);
+        SLIST_INSERT_HEAD(&head, new_conn, entries);
+
+        //finish handling new connection by cleaning up any completed old connections
+        pthread_mutex_lock(&list_mutex);
+
+        node = SLIST_FIRST(&head);
+        while (node != NULL){
+            next_node = SLIST_NEXT(node, entries);
+            if (node->is_complete == 1){
+                pthread_join(node->thread_id, NULL);
+                SLIST_REMOVE(&head, node, connection_node, entries);
+                free(node);
             }
-
-            if(read_count)
-                send(their_fd, buffer, read_count, 0);
-
-        } while (read_count);
-
-        close(readfile_fd);
-        close(their_fd);
-        syslog(LOG_INFO, "Closed connection from %s", ip_string);
+            node = next_node;
+        }
+        
+        pthread_mutex_unlock(&list_mutex);
+        
     }
-
+    
+    node = SLIST_FIRST(&head);
+    while (node != NULL){
+        if(!node->is_complete){
+            shutdown(node->client_fd, SHUT_RDWR);
+        }
+        next_node = SLIST_NEXT(node, entries);
+        pthread_join(node->thread_id, NULL);
+        SLIST_REMOVE(&head, node, connection_node, entries);
+        free(node);
+        node = next_node;
+    }
+    pthread_join(timer_thread, NULL);
     remove(WRITE_FILE);
     close(sock_fd);
-    free(buffer);
     freeaddrinfo(servinfo);
+    pthread_mutex_destroy(&list_mutex);
+    pthread_mutex_destroy(&file_write_mutex);
+
     return 0;
 }
